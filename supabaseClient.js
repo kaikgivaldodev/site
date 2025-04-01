9c9d14d18378bf479ff567a376669388f39b7cdd
kaikgivaldodev <contatogivaldo@outlook.com>
2025-03-31 21:13:30 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Substitua pelos seus valores da Supabase
-const supabaseUrl = 'https://<SEU-PROJETO>.supabase.co';
-const supabaseAnonKey = '<SUA-CHAVE-ANONIMA>';
+const supabaseUrl = 'https://ulkkrprfksmydrpzwnnz.supabase.co';
+const supabaseAnonKey = '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsa2tycHJma3NteWRycHp3bm56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0NjQ3MzMsImV4cCI6MjA1OTA0MDczM30.vtx34avkzHpIbXvis1qdcalCBcb0BSrwOFJiEOcD2Ks>';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
